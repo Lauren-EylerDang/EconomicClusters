b0a1fe4bda45295740c272bb1b870d04385d0ad1
Lauren-Eyler <lauren.e.eyler@gmail.com>
2019-07-10 18:28:03 -0700

    produced a data frame of factors

--- R/EC_vars.R
+++ R/EC_vars.R
@@ -3,6 +3,7 @@ EC_vars<-function(X, p){
   common_names<-colnames(assets_num[,colMeans(assets_num, na.rm=TRUE)>p])
   pop<-X[,colnames(X) %in% common_names==TRUE]
   pop<-lapply(pop, as.factor)
+  pop<-as.data.frame(pop)
   return(pop)
   #' @export
 }
